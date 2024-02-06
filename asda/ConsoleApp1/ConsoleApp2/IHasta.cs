b642b5ab3f30671fecaac3ac8ba743c03a00ee93
Sumeyye Coskun <62007900+smyy96@users.noreply.github.com>
2024-02-06 19:01:48 +0300

    Add files via upload

---
+++ asda/ConsoleApp1/ConsoleApp2/IHasta.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp2
+{
+    public interface IHasta
+    {
+        public List<IRandevu> Randevular { get; set; }
+        public List<IHastalık> Hastalıklar { get; set; }
+    }
+}
---
+++ asda/ConsoleApp1/ConsoleApp2/Kullanici.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp2
+{
+    public abstract class  Kullanici
+    {
+
+        public Kullanici()
+        {
+            _id = new Random().Next(300, 9999);
+
+           
+        }
+
+        public Kullanici(int id)
+        {
+            _id = id;
+        }
+        //
+        private string _ad;
+        private string _tc;
+        private int _id;
+        public int Id { get {return _id; } }
+        public string Ad { 
+            get { 
+                return _ad; 
+            }
+            set
+            {
+                if (value.Length >30)
+                {
+                    throw new Exception("kullanıcı adı sınırı");
+                }
+                _ad = value;    
+            }
+        
+        }
+        public string SoyAd { get; set; }
+
+        public string TCNumara {
+            get
+            {
+                return TcSansürle(_tc);
+            } 
+            set
+            {
+                _tc = value;
+            }
+        
+        }
+        public DateTime  DoğumTarihi { get; set; }
+        public int Boy { get; set; }
+        public int Kilo { get; set; }
+        public Cinsiyet Cinsiyet { get; set; }
+        public KanGrubu KanGrubu { get; set; }
+
+        private string TcSansürle(string tc) => tc.Substring(0, 7) + "****";
+
+
+            
+
+    }
+}
